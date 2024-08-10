d36d51eff7128926e8813176332b25d2c56eb989
Adrian T <adrian.toscanoang@gmail.com>
2024-08-10 12:21:27 -0700

    implemented view playlist when not empty

--- ViewPlaylist.py
+++ ViewPlaylist.py
@@ -10,7 +10,8 @@ def viewPlaylistScreen():
     clearScreen()
     print("------------------------------- Your Playlist -------------------------------\n")
 
-    print(request_playlist())
+    # invokes the GetPlaylist microservice
+    request_playlist()
 
     # display options
     addNewLine(2)
@@ -27,13 +28,16 @@ def request_playlist():
     socket.connect("tcp://localhost:4233")
 
     # send request
-    request = "G"
+    request = "GET"
     byte_request = request.encode('utf-8')
     socket.send(byte_request)
 
     # decode reply when received
     byte_response = socket.recv()
-    playlist = byte_response.decode('utf-8')
+    json_data = byte_response.decode('utf-8')
+    playlist = json.loads(json_data)
 
-    # return the playlist
-    return playlist
+    # print every song in the playlist
+    for song in playlist:
+        print("\t", end="")
+        print(song)
